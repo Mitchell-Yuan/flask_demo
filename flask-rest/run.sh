a75e9f47aa54db6510e921d575fa75e23c6daf8a
Z19070946 <Mitchell_Yuan@wistron.com>
2021-01-11 13:17:05 +0800

    update

---
+++ flask-rest/run.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env sh
+
+# rm /etc/nginx/nginx.conf
+# cd /flask-rest
+# cp nginx.conf /etc/nginx/
+
+# service nginx start
+# service nginx status
---
+++ start.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env sh
+set -e
+/bin/bash /etc/profile
+
+cd /flask-rest
+git pull
+
+# /bin/bash run.sh
